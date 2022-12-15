2a62c18a3af58ea0641d93d95cc55262046c9158
DeanH1990 <dhasley1990@gmail.com>
2022-12-15 18:28:16 +0000

    setup connection to mongodb

---
+++ database/connection.ts
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const ENV = process.env.NODE_ENV || "dev";
+
+require("dotenv").config({
+  path: `${__dirname}/../.env.${ENV}`,
+});
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL not set");
+}
+//
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.DATABASE_URL);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
+
+
+module.exports = connectDB;
