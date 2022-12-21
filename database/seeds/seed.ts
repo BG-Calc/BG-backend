14c7ad855e2f5defc25096862073b11cfa1257e6
DeanH1990 <dhasley1990@gmail.com>
2022-12-21 17:14:37 +0000

    add close DB fn for test file

--- database/seeds/seed.ts
+++ database/seeds/seed.ts
@@ -1,6 +1,7 @@
 import { User } from "./Models/usersModel";
 import { Games } from "./Models/gamesModel";
 import { Faction } from "./Models/factionsModel";
+import mongoose from "mongoose";
 
 export const seedDatabase = async (factionData: typeof Faction, usersData: typeof User, gamesData: typeof Games) => {
     await Faction.deleteMany({});
@@ -9,4 +10,8 @@ export const seedDatabase = async (factionData: typeof Faction, usersData: typeo
     await User.insertMany(usersData);
     await Games.deleteMany({});
     await Games.insertMany(gamesData)
+}
+
+export const closeDB = async () => {
+    await mongoose.connection.close()
 }
